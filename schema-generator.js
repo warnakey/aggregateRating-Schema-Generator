b7fb1fc98072d64ac070593e4567de5503a34802
warnakey <ericwarncke@gmail.com>
2019-03-07 21:17:08 -0500

    Update schema-generator.js

--- schema-generator.js
+++ schema-generator.js
@@ -2,20 +2,41 @@
 
 "use strict";
 
+///////////////////////////////////////////////////////////////////////////////////////////////
+//                         ADD OR DELETE BUTTONS (Alt name & Reviews)                        //
+///////////////////////////////////////////////////////////////////////////////////////////////
+
 jQuery(document).ready(function() {
-	let schema_count = 1;
-	// whenever ADD SCHEMA REVIEW button is clicked
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	//                                      ADD REVIEWS                                        //
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// make a variable to count the number of reviews
+	let schema_count = 0;
+	
+	// schema_count starts at zero, so add 1 to it to show the number of reviews visually starting at 1
+	let schema_count_plus_one = schema_count + 1;
+		
+	// whenever ADD REVIEW button is clicked
 	$('.addreviewschema').click(function(e) {
+		
+		// make a variable for the add reviews button container
+		let addReviewsButton2 = document.getElementById("addreviewsul");
+			
+		// if there are less than 5000 reviews, allow the add reviews button to be seen
+		addReviewsButton2.style.display = "block";
+		
 		e.preventDefault();
 		// add the following code inside the INDIVIDUAL REVIEWS div
 		$(".individualreviews").append(
 
 			// opening tag
-			'<li><div class="footer-schema">'
-		  + '<h4>REVIEW #' + schema_count + '</h4>'
+			'<li class="reviewli' + schema_count + '">'
+		  + '<div class="footer-schema"><div class="titlebar"><h4>Review ' + schema_count_plus_one + '</h4></div>'
 
 			// review author 
-		  + '<div class="questionSixteen"><p>What is the name of the person that wrote the review?</p><label for="reviewauthor' + schema_count + '">Reviewer\'s Name: </label><input type="text" id="reviewauthor' + schema_count + '" name="reviewauthor' + schema_count + '" onKeyPress="return noenter()"></div>'
+		  + '<div class="reviewsquestions"><div class="questionSixteen"><p>What is the name of the person that wrote the review?</p><label for="reviewauthor' + schema_count + '">Reviewer\'s Name: </label><input type="text" id="reviewauthor' + schema_count + '" name="reviewauthor' + schema_count + '" onKeyPress="return noenter()"></div>'
 
 			// review date
 		  + '<div class="questionSeventeen"><p>What is the date that the review was written?</p><label for="reviewdate' + schema_count + '">Review Date: </label><input type="text" id="reviewdate' + schema_count + '" name="reviewdate' + schema_count + '" onKeyPress="return noenter()"></div>'
@@ -25,17 +46,33 @@ jQuery(document).ready(function() {
 
 			// review score
 		  + '<div class="questionNineteen"><p>What is the score that the reviewer left?</p><label for="reviewscore' + schema_count + '">Reviewer\'s Score: </label><!-- write a rule later that says IF the score left below is higher than question 14, make the form spit an error message --><input type="text" id="reviewscore' + schema_count + '" name="reviewscore' + schema_count + '" value="100" onKeyPress="return noenter()"></div>'
-
-			// closing tag
-		  + '</div><br>'
-
-			// this is the thing people will click to delete the review if they choose
-		  + '<a href="#" class="deletereviewschema">Delete Review</a></li>');
+		  
+		  // this is the thing people will click to delete the review if they choose
+		  + '</div></div><a href="#" class="deletereviewschema">Delete</a></li>'
+		  
+		);
 
 		// increase the schema count by 1 every time someone clicks the ADD REVIEW button
 		schema_count++;
+		schema_count_plus_one++;
+		
+		// only allow up to 10 alternate names
+		if (schema_count > 4999) {
+			
+			let schema_count = 4999;
+			
+			// make a variable for the add alternate names button
+			let addReviewsButton = document.getElementById("addReviewsButton");
+			
+			// if there are 10 alternate names fields, make the add alternate names button disappear
+			addReviewsButton.style.display = "none";
+		}
 		
 	});
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	//                                     DELETE REVIEWS                                      //
+	/////////////////////////////////////////////////////////////////////////////////////////////
 
 	// if someone clicks the DELETE REVIEW button, do this
 	$('.individualreviews').on('click', '.deletereviewschema', function(f) {
@@ -44,47 +81,157 @@ jQuery(document).ready(function() {
 		$(this).parent().remove();
 		
 		schema_count--;
-
-		if (schema_count <= 0) {
-			let schema_count = 1;
+		schema_count_plus_one -= 1;
+		
+		if (schema_count >= 0 && schema_count <= 4999) {
+					
+			// make a variable for the add review button
+			let addReviewsButton = document.getElementById("addReviewsButton");
+			
+			// make a variable for the add review button container
+			let addReviewsButton2 = document.getElementById("addmorereviewscontainer");
+			
+			// make a variable for the add review button container
+			let addReviewsButton3 = document.getElementById("addreviewsul");
+				
+			// once there are 4999 reviews, make the add reviews button disappear
+			addReviewsButton.style.display = "block";
+			addReviewsButton2.style.display = "block";
+			addReviewsButton3.style.display = "block";
+			
+		// don't allow the number of reviews to be less than 1
+		} else if (schema_count < 0) {
+			
+			let schema_count = 0;
+			
+		// don't allow the number of reviews to be more than 5000
+		} else if (schema_count > 4999) {
+			
+			let schema_count = 4999;
 		}
+		
 	});
-
-	// TESTING REMOVE LATER
-	/*$('.add_project_file').click(function(g) {
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	//                                      ADD ALT NAMES                                      //
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// make a variable to count the number of alternate names
+	let altname_count = 0;
+	
+	// altname_count starts at zero, so add 1 to it to show the number of alt names visually starting at 1
+	let altname_count_plus_one = altname_count + 1;
+		
+	// whenever ADD ALT NAME button is clicked
+	$('.addaltNames').click(function(g) {
+		
+		
+		// make a variable for the add alternate names button container
+		let addAltNamesButton3 = document.getElementById("altnamesul");
+			
+		// if there are less than 10 alt name fields, allow the add alt names button to be seen
+		addAltNamesButton3.style.display = "block";
+		
 		g.preventDefault();
-		$(".project_images").append(
-			'<li>'
-		  + '<a href="#" class="remove_project_file" border="2" style="float: left;width: 100%;">DELETE</a>'
-		  + '</li>');
+		// add the following code inside the ALT NAMES div
+		$(".altNames").append(
+
+			// opening li
+			'<li class="reviewli' + altname_count + '">'
+
+			// alt name label and input 
+		  + '<label for="altschemaname' + altname_count + '">Alt Name ' + altname_count_plus_one + ': </label><input type="text" id="altschemaname' + altname_count + '" name="altschemaname" onKeyPress="return noenter()"><br>'
+
+			// this is the thing people will click to delete the alt name if they choose
+		  + '<a href="#" class="deletealtname">Delete</a></li>'
+		  
+		);
+		
+		// increase the schema count by 1 every time someone clicks the ADD REVIEW button
+		altname_count++;
+		altname_count_plus_one++;
+		
+		// after someone clicks on the "add alt name" button, create a variable for the alt name
+		let altSchemaName = document.forms["schemageneratorform"]["altschemaname"].value; // alternate name
+		
+		// only allow up to 10 alternate names
+		if (altname_count > 9) {
+			
+			let altname_count = 9;
+			
+			// make a variable for the add alternate names button
+			let addAltNamesButton = document.getElementById("addAltNamesButton");
+			
+			// once the user clicks on the add alternate names button 10 times, make the add alternate names button disappear
+			addAltNamesButton.style.display = "none";
+		}
+		
 	});
-	// Remove parent of 'remove' link when link is clicked.
-	$('.project_images').on('click', '.remove_project_file', function(h) {
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	//                                    DELETE ALT NAMES                                     //
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// if someone clicks the DELETE REVIEW button, do this
+	$('.altNames').on('click', '.deletealtname', function(h) {
 		h.preventDefault();
 
 		$(this).parent().remove();
-	});*/
+		
+		altname_count -= 1;
+		altname_count_plus_one -= 1;
+		
+		if (altname_count <= 10) {
+					
+			// make a variable for the add alternate names button
+			let addAltNamesButton = document.getElementById("addAltNamesButton");
+			
+			// make a variable for the add alternate names button container
+			let addAltNamesButton2 = document.getElementById("addAltNamesButtonContainer");
+			
+			// make a variable for the add alternate names button container
+			let addAltNamesButton3 = document.getElementById("altnamesul");
+				
+			// once the user clicks on the add alternate names button 10 times, make the add alternate names button disappear
+			addAltNamesButton.style.display = "block";
+			addAltNamesButton2.style.display = "block";
+			addAltNamesButton3.style.display = "block";
+			
+		} else if (altname_count < 0) {
+			
+			let altname_count = 0;
+			
+		} else if (altname_count > 10) {
+			
+			let altname_count = 10;
+		}
+	});
+	
 });
 
-
-// Remove parent of 'remove' link when link is clicked.
-$('.project_images').on('click', '.remove_project_file', function(e) {
-    e.preventDefault();
-
-    $(this).parent().remove();
-});
-
+// reset the page when the reset button is clicked
 function resetpage() {
   location.reload();
 }
 
-// this function let's question 2 appear when a bullet is clicked
+///////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                           //
+//                      HIDE OR DISPLAY QUESTIONS BASED ON SCHEMA TYPE                       //
+//                                                                                           //
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+// this function allows question 2 and beyond to appear when a bullet is clicked
 function nextQuestion() {
+	
+	// make variables that contains user inputs
+	//              check document,[   form name=""     ][input name=""].value
+	let schemaType = document.forms["schemageneratorform"]["schematype"].value; // schema type
+	
 	// This creates an array of the 3 schema type choices
 	let schematypes = document.getElementsByName("schematype");
 	
 	// if the schema type is LOCAL BUSINESS
-	if (schematypes[0].checked == true) {
+	if (schemaType == "localbusiness") {
 	
 		// make a variable for the div that contains question 2 and beyond
 		let nextBlock = document.getElementById("nextBlock");
@@ -103,7 +250,7 @@ function nextQuestion() {
 		document.getElementById('schematypechoice2').innerHTML = "Local Business";
 		document.getElementById('schematypechoice3').innerHTML = "Local Business";
 		
-	} else if (schematypes[1].checked == true) {
+	} else if (schemaType == "product") {
 	
 		// make a variable for the div that contains question 2
 		let nextBlock = document.getElementById("nextBlock");
@@ -122,7 +269,7 @@ function nextQuestion() {
 		// once the user clicks on the product bullet, make the product condition question appear
 		productcondition.style.display = "block";
 		
-	} else if (schematypes[2].checked == true) {
+	} else if (schemaType == "service") {
 	
 		// make a variable for the div that contains question 2
 		let nextBlock = document.getElementById("nextBlock");
@@ -157,6 +304,12 @@ function noenter() {
 	return !(window.event && window.event.keyCode == 13);
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                           //
+//                      OUTPUT VALID SCHEMA BASED ON THE USER'S CHOICES                      //
+//                                                                                           //
+///////////////////////////////////////////////////////////////////////////////////////////////
+
 // the final output when the submit button is pressed
 function outputSchema() {
 
@@ -173,11 +326,14 @@ function outputSchema() {
 	//              check document,[   form name=""     ][input name=""].value
 	let schemaType = document.forms["schemageneratorform"]["schematype"].value; // schema type
 	let schemaName = document.forms["schemageneratorform"]["schemaname"].value; // product/service/business name
-	let altSchemaName = document.forms["schemageneratorform"]["altschemaname"].value; // alternate name
 	let schemaCondition = document.forms["schemageneratorform"]["schemacondition"].value; // condition
 
 	// This creates an array of the 3 schema type choices
 	let schematypes = document.getElementsByName("schematype");
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	//                                 LOCAL BUSINESS SCHEMA                                   //
+	/////////////////////////////////////////////////////////////////////////////////////////////
 	
 	// if the schema type is LOCAL BUSINESS
 	if (schemaType == "localbusiness") {
@@ -201,6 +357,9 @@ function outputSchema() {
 		// inside the output box, make it say the schemaType
 		document.getElementById('output').innerHTML = chosenSchemaType;
 		
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	//                                     PRODUCT SCHEMA                                      //
+	/////////////////////////////////////////////////////////////////////////////////////////////
 		
 	// if the schema type is PRODUCT
 	} else if (schemaType == "product") {
@@ -419,6 +578,10 @@ function outputSchema() {
 
 		// give people the option to get their schema export emailed to them if they wish
 		document.getElementById('emailoutput').innerHTML = emailtext;
+		
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	//                                    SERVICE SCHEMA                                       //
+	/////////////////////////////////////////////////////////////////////////////////////////////
 		
 	// if the schema type is SERVICE
 	} else if (schemaType == "service") {
